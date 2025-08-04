9cb1e634ed0b717bd404561c8cfd2046fcde15b4
abdulwahab <abdulwahabmahmoud99@gmail.com>
2025-08-04 23:15:18 +0300

    enhance: Refactor AllProjects component layout and structure

--- src/layouts/pages/profile/all-projects/index.js
+++ src/layouts/pages/profile/all-projects/index.js
@@ -74,123 +74,135 @@ function AllProjects() {
   );
 
   return (
-    <MDBox pb={3}>
-      <Grid container alignItems="center">
-        <Grid item xs={12} md={7}>
-          <MDBox mb={1}>
-            <MDTypography variant="h5">
-              Some of Our Awesome Projects
-            </MDTypography>
-          </MDBox>
-          <MDBox mb={2}>
-            <MDTypography variant="body2" color="text">
-              This is the paragraph where you can write more details about your
-              projects. Keep you user engaged by providing meaningful
-              information.
-            </MDTypography>
-          </MDBox>
-        </Grid>
-        <Grid item xs={12} md={5} sx={{ textAlign: "right" }}>
-          <MDButton variant="gradient" color="info">
-            <Icon>add</Icon>&nbsp; Add New
-          </MDButton>
-        </Grid>
-      </Grid>
-      <MDBox mt={5}>
-        <Grid container spacing={3}>
-          <Grid item xs={12} md={6} lg={4}>
-            <MDBox mb={1.5} mt={1.5}>
-              <ComplexProjectCard
-                image={logoSlack}
-                title="slack bot"
-                description="If everything I did failed - which it doesn't, I think that it actually succeeds."
-                dateTime="02.03.22"
-                members={[team1, team2, team3, team4, team5]}
-                dropdown={{
-                  action: openSlackBotMenu,
-                  menu: renderMenu(slackBotMenu, closeSlackBotMenu),
-                }}
-              />
+    <>
+      <Header />
+      <MDBox pb={3}>
+        <Grid container alignItems="center">
+          <Grid item xs={12} md={7}>
+            <MDBox mb={1}>
+              <MDTypography variant="h5">
+                Some of Our Awesome Projects
+              </MDTypography>
+            </MDBox>
+            <MDBox mb={2}>
+              <MDTypography variant="body2" color="text">
+                This is the paragraph where you can write more details about
+                your projects. Keep you user engaged by providing meaningful
+                information.
+              </MDTypography>
             </MDBox>
           </Grid>
-          <Grid item xs={12} md={6} lg={4}>
-            <MDBox mb={1.5} mt={1.5}>
-              <ComplexProjectCard
-                image={logoSpotify}
-                title="premium support"
-                description="Pink is obviously a better color. Everyone’s born confident, and everything’s taken away from you."
-                dateTime="22.11.21"
-                members={[team1, team2, team3]}
-                dropdown={{
-                  action: openPremiumSupportMenu,
-                  menu: renderMenu(premiumSupportMenu, closePremiumSupportMenu),
-                }}
-              />
-            </MDBox>
-          </Grid>
-          <Grid item xs={12} md={6} lg={4}>
-            <MDBox mb={1.5} mt={1.5}>
-              <ComplexProjectCard
-                image={logoXD}
-                title="design tools"
-                description="Constantly growing. We’re constantly making mistakes from which we learn and improve."
-                dateTime="06.03.20"
-                members={[team1, team2, team3, team4]}
-                dropdown={{
-                  action: openDesignToolsMenu,
-                  menu: renderMenu(designToolsMenu, closeDesignToolsMenu),
-                }}
-              />
-            </MDBox>
-          </Grid>
-          <Grid item xs={12} md={6} lg={4}>
-            <MDBox mb={1.5} mt={1.5}>
-              <ComplexProjectCard
-                image={logoAsana}
-                title="looking great"
-                description="You have the opportunity to play this game of life you need to appreciate every moment."
-                dateTime="14.03.24"
-                members={[team1, team2, team3, team4, team5, team3]}
-                dropdown={{
-                  action: openLookingGreatMenu,
-                  menu: renderMenu(lookingGreatMenu, closeLookingGreatMenu),
-                }}
-              />
-            </MDBox>
-          </Grid>
-          <Grid item xs={12} md={6} lg={4}>
-            <MDBox mb={1.5} mt={1.5}>
-              <ComplexProjectCard
-                image={logoInvision}
-                title="developer first"
-                description="For standing out. But the time is now to be okay to be the greatest you."
-                dateTime="16.01.22"
-                members={[team1, team2, team3, team4]}
-                dropdown={{
-                  action: openDeveloperFirstMenu,
-                  menu: renderMenu(developerFirstMenu, closeDeveloperFirstMenu),
-                }}
-              />
-            </MDBox>
-          </Grid>
-          <Grid item xs={12} md={6} lg={4}>
-            <MDBox mb={1.5} mt={1.5}>
-              <ComplexProjectCard
-                image={logoAtlassian}
-                title="Product Development"
-                description="We strive to embrace and drive change in our industry. We are happy to work at such a project."
-                dateTime="16.01.22"
-                members={[team1, team2, team3, team4]}
-                dropdown={{
-                  action: openDeveloperFirstMenu,
-                  menu: renderMenu(developerFirstMenu, closeDeveloperFirstMenu),
-                }}
-              />
-            </MDBox>
+          <Grid item xs={12} md={5} sx={{ textAlign: "right" }}>
+            <MDButton variant="gradient" color="info">
+              <Icon>add</Icon>&nbsp; Add New
+            </MDButton>
           </Grid>
         </Grid>
+        <MDBox mt={5}>
+          <Grid container spacing={3}>
+            <Grid item xs={12} md={6} lg={4}>
+              <MDBox mb={1.5} mt={1.5}>
+                <ComplexProjectCard
+                  image={logoSlack}
+                  title="slack bot"
+                  description="If everything I did failed - which it doesn't, I think that it actually succeeds."
+                  dateTime="02.03.22"
+                  members={[team1, team2, team3, team4, team5]}
+                  dropdown={{
+                    action: openSlackBotMenu,
+                    menu: renderMenu(slackBotMenu, closeSlackBotMenu),
+                  }}
+                />
+              </MDBox>
+            </Grid>
+            <Grid item xs={12} md={6} lg={4}>
+              <MDBox mb={1.5} mt={1.5}>
+                <ComplexProjectCard
+                  image={logoSpotify}
+                  title="premium support"
+                  description="Pink is obviously a better color. Everyone’s born confident, and everything’s taken away from you."
+                  dateTime="22.11.21"
+                  members={[team1, team2, team3]}
+                  dropdown={{
+                    action: openPremiumSupportMenu,
+                    menu: renderMenu(
+                      premiumSupportMenu,
+                      closePremiumSupportMenu
+                    ),
+                  }}
+                />
+              </MDBox>
+            </Grid>
+            <Grid item xs={12} md={6} lg={4}>
+              <MDBox mb={1.5} mt={1.5}>
+                <ComplexProjectCard
+                  image={logoXD}
+                  title="design tools"
+                  description="Constantly growing. We’re constantly making mistakes from which we learn and improve."
+                  dateTime="06.03.20"
+                  members={[team1, team2, team3, team4]}
+                  dropdown={{
+                    action: openDesignToolsMenu,
+                    menu: renderMenu(designToolsMenu, closeDesignToolsMenu),
+                  }}
+                />
+              </MDBox>
+            </Grid>
+            <Grid item xs={12} md={6} lg={4}>
+              <MDBox mb={1.5} mt={1.5}>
+                <ComplexProjectCard
+                  image={logoAsana}
+                  title="looking great"
+                  description="You have the opportunity to play this game of life you need to appreciate every moment."
+                  dateTime="14.03.24"
+                  members={[team1, team2, team3, team4, team5, team3]}
+                  dropdown={{
+                    action: openLookingGreatMenu,
+                    menu: renderMenu(lookingGreatMenu, closeLookingGreatMenu),
+                  }}
+                />
+              </MDBox>
+            </Grid>
+            <Grid item xs={12} md={6} lg={4}>
+              <MDBox mb={1.5} mt={1.5}>
+                <ComplexProjectCard
+                  image={logoInvision}
+                  title="developer first"
+                  description="For standing out. But the time is now to be okay to be the greatest you."
+                  dateTime="16.01.22"
+                  members={[team1, team2, team3, team4]}
+                  dropdown={{
+                    action: openDeveloperFirstMenu,
+                    menu: renderMenu(
+                      developerFirstMenu,
+                      closeDeveloperFirstMenu
+                    ),
+                  }}
+                />
+              </MDBox>
+            </Grid>
+            <Grid item xs={12} md={6} lg={4}>
+              <MDBox mb={1.5} mt={1.5}>
+                <ComplexProjectCard
+                  image={logoAtlassian}
+                  title="Product Development"
+                  description="We strive to embrace and drive change in our industry. We are happy to work at such a project."
+                  dateTime="16.01.22"
+                  members={[team1, team2, team3, team4]}
+                  dropdown={{
+                    action: openDeveloperFirstMenu,
+                    menu: renderMenu(
+                      developerFirstMenu,
+                      closeDeveloperFirstMenu
+                    ),
+                  }}
+                />
+              </MDBox>
+            </Grid>
+          </Grid>
+        </MDBox>
       </MDBox>
-    </MDBox>
+    </>
   );
 }
 
